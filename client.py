602af6519620902bfa02ee719b0de7af4937b1a6
Ben Randoing <benjamin.randoing@duke.edu>
2022-03-02 15:33:09 -0500

    client

---
+++ client.py
@@ -0,0 +1,16 @@
+import requests
+
+
+server = "http://127.0.0.1:5000"
+
+# r = requests.get("http://127.0.0.1:5000/")
+# print(r.status_code)
+# print(r.text)
+
+out_data = {"a": 3, "b": 5}
+
+r = requests.post(server + "/hdl_check", json=out_data)
+print(r.status_code)
+print(r.text)
+a = r.json()
+print(a[0])
